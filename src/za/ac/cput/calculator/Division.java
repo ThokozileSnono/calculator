ba99681d12b2536449243f8e7c7fb1b12618f073
Thokozile Snono <thokozilesinono>
2021-03-10 22:08:50 +0200

    Second commit

--- src/za/ac/cput/calculator/Division.java
+++ src/za/ac/cput/calculator/Division.java
@@ -2,8 +2,6 @@ package za.ac.cput.calculator;
 
 /**
  * Name and Surname: Thokozile Snono
- * Student number: 216032121
- * Application returns result of integer division
  */
 
 public class Division {
